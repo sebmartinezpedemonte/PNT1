98f16fd1e3b302805779750d119c93f0e4c30908
IGNACIO EDUARDO PAUSA <28231411@comunidadort.edu.ar>
2022-09-12 11:06:27 -0300

    Se agregó carpeta js y validación de contraseña

---
+++ TPCarta2/js/validacionFormulario.js
@@ -0,0 +1,12 @@
+function validacionContrasena(){
+    var contrasena = document.getElementById('pwd');
+    var repetir = document.getElementById('pwd-repeat');
+
+    if(contrasena == repetir){
+        return true;
+    }else{
+        alert('Las contraseñas deben ser iguales!!!');
+        pwd-repeat.focus();
+        return false;
+    }
+}
