a5136b67049c9b9722b66aae3d7f5993f10ef3d8
Felix Lamas <felix.lamas@hotmail.com>
2022-09-26 21:41:06 -0300

    commit

--- TPCarta2/js/validacionFormulario.js
+++ TPCarta2/js/validacionFormulario.js
@@ -62,3 +62,19 @@ function ValidaContrasena(contrasena, repetir) {
         return false;
     }
 }
+
+function ValidaFechaNac() {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0 so need to add 1 to make it 1!
+    var yyyy = today.getFullYear();
+    if(dd<10){
+      dd='0'+dd
+    } 
+    if(mm<10){
+      mm='0'+mm
+    } 
+    
+    today = yyyy+'-'+mm+'-'+dd;
+    document.getElementById("datefield").setAttribute("max", today);
+}
